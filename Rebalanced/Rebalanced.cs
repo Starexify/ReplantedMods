dab16ef079b5c70c3d7dce7f8c7639de719d43ae
ImVeryBad <80699736+Starexify@users.noreply.github.com>
2025-11-01 14:41:51 +0100

    Some more small rebalances to make Kernelpult better

--- Rebalanced/Rebalanced.cs
+++ Rebalanced/Rebalanced.cs
@@ -1,5 +1,6 @@
 ﻿using BadAPI.events;
 using HarmonyLib;
+using Il2CppReloaded.Data;
 using Il2CppReloaded.Gameplay;
 using Il2CppReloaded.TreeStateActivities;
 using MelonLoader;
@@ -13,12 +14,41 @@ public class Rebalanced : MelonMod
 {
     public override void OnInitializeMelon()
     {
+        // Slower fire
+        PlantEvents.OnInitPost += (plant, gridX, gridY, seedType, imitaterType, controller) =>
+        {
+            if (seedType == SeedType.Kernelpult) plant.mLaunchRate = 475; // Original: 300
+        };
+
+        // Always hit with butter
         PlantEvents.OnShoot += (Plant plant, Zombie targetZombie, int row, ref PlantWeapon plantWeapon) =>
         {
             if (plant.mSeedType == SeedType.Kernelpult) plantWeapon = PlantWeapon.Secondary;
         };
     }
 
+    /*[HarmonyPatch(typeof(PlantDefinition))]
+    internal static class PlantDefinitionPatch
+    {
+        [HarmonyPatch("get_LaunchRate")]
+        [HarmonyPostfix]
+        internal static void LaunchRatePostfix(PlantDefinition __instance, ref int __result)
+        {
+            if (__instance.m_seedType == SeedType.Kernelpult) __result = 1000;
+        }
+    }*/
+
+    // Patch Almanac Entry cost
+    [HarmonyPatch(typeof(AlmanacEntryData), "get_EntrySunCost")]
+    internal static class AlmanacEntryDataPatch
+    {
+        internal static void Postfix(AlmanacEntryData __instance, ref float __result)
+        {
+            if (__instance.m_seedType == SeedType.Kernelpult) __result = 225;
+        }
+    }
+
+    // Increase plant cost and lower refresh time
     [HarmonyPatch(typeof(Plant))]
     internal static class PlantPatch
     {
@@ -26,10 +56,7 @@ public class Rebalanced : MelonMod
         internal static void Postfix(GameplayActivity gLawnApp, SeedType theSeedType, SeedType theImitaterType,
             ref int __result)
         {
-            if (theSeedType == SeedType.Kernelpult)
-            {
-                __result = 225;
-            }
+            if (theSeedType == SeedType.Kernelpult) __result = 225; // Original: 100
         }
 
         [HarmonyPatch(nameof(Plant.GetRefreshTime))]
@@ -37,10 +64,7 @@ public class Rebalanced : MelonMod
         internal static void RefreshPost(GameplayActivity app, SeedType theSeedType, SeedType theImitaterType,
             ref int __result)
         {
-            if (theSeedType == SeedType.Kernelpult)
-            {
-                __result = 2250;
-            }
+            if (theSeedType == SeedType.Kernelpult) __result = 2250; // Original: 750
         }
     }
 }
